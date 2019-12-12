f2353c159cc41f90cee18fb8724a53a14549b456
Shiv Alagar <ashivalagar@gmail.com>
2019-12-12 10:19:17 +0800

    Clean up packages and file structure

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <p>Template file structure to begin with</p>
+        );
+    }
+}
+
+export default HomePage;
