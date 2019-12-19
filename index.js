5beafb14cf9691b64fccb875e7fe6485a0c4c39c
Shiv Alagar <ashivalagar@gmail.com>
2019-12-19 22:08:22 +0530

    Updated Jquery CDN

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 }
-//for arrow click part (PART2)
 $(document).ready(function () {
     $('#down').on('click', function () {
         $("html").scrollTop(0);
